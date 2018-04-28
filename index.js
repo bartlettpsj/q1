0c78f4fe4ed4cbdc3d941ef94c11a8a19d5371d9
Paul Bartlett <paul.bartlett@everyonecounts.com>
2018-04-27 21:03:05 -0700

    p2

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // This test will load an inage file and the run through barcode read and display the contents using different technolgoies
 
 // c1 - p1
-// c2
+// c2 - p2
 // c3
 
 const Quagga = require('quagga/lib/quagga').default;
